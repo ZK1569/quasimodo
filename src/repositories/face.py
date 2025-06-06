539cf1e283c28213a938631b6ed605e04e798845
Cristian URSU <cristian.ursu.2001@gmail.com>
2025-06-06 12:59:50 -0400

    update env.example

--- src/repositories/face.py
+++ src/repositories/face.py
@@ -2,16 +2,18 @@ from typing import Generator
 
 from src.utils.config import postgresql_database
 from src.repositories.repository import FaceRepositoryAbs
-   
+
+
 class FaceRepository(FaceRepositoryAbs):
     def __init__(self):
         pass
-    
+
     def select_faces(self):
         with postgresql_database.cursor_context() as cursor:
-            cursor.execute("SELECT id, firstname, lastname, face_embeddings FROM face")
+            cursor.execute(
+                "SELECT id, firstname, lastname, face_embeddings FROM face")
             return cursor.fetchall()
-    
+
     def insert_face(self, firstname, lastname, face_embedding):
         print(f"Inserting face: {firstname} {lastname}")
         try:
@@ -24,6 +26,8 @@ class FaceRepository(FaceRepositoryAbs):
         except Exception as e:
             print(f"Insertion failed: {e}")
             return False
-        
+
+
 def get_face_repository() -> Generator[FaceRepositoryAbs, None, None]:
     yield FaceRepository()
+
